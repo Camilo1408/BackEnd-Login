dd4b8fa82593c0031ae62270cdf4b1704121be68
Cesar Camilo <cesarcamilohoyosdiaz@gmail.com>
2024-05-06 17:26:27 -0500

    Backend Login

---
+++ src/main/java/cechdi/Backendlogin/User/Role.java
@@ -0,0 +1,6 @@
+package cechdi.Backendlogin.User;
+
+public enum Role {
+	ADMIN,
+	USER
+}
---
+++ src/test/java/cechdi/Backendlogin/BackendLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package cechdi.Backendlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
